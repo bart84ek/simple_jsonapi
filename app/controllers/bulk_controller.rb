1e0071e19f8b035c788ad234c783040e7761bb3c
BartekDymowski <bart.dy@gmail.com>
2021-12-16 17:32:57 +0100

    Define strong parameters for bulk operations

--- app/controllers/bulk_controller.rb
+++ app/controllers/bulk_controller.rb
@@ -1,6 +1,27 @@
 # BulkController
 class BulkController < ApplicationController
+  include Jsonapi::Deserializer
+
   def operations
     render json: { ok: true }
+  end
+
+  private
+
+  def article_attributes
+    [:title]
+  end
+
+  def author_attributes
+    [:name]
+  end
+
+  def bulk_params
+    attributes = article_attributes + author_attributes
+    relation_attributes = { data: [:type, :lid] }
+    relationships = [author: relation_attributes]
+    data = [:type, :lid, { attributes: attributes }, { relationships: relationships }]
+    operations = [:op, { ref: [:type] }, { data: data }]
+    params.require(:bulk).permit(operations: operations)
   end
 end
