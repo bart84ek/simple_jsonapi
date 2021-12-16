ccc6504b4d8550d8fdd2fb8385d49c27001660f7
BartekDymowski <bart.dy@gmail.com>
2021-12-16 08:57:12 +0100

    Added integration tests to bulk controller

--- test/controllers/bulk_controller_test.rb
+++ test/controllers/bulk_controller_test.rb
@@ -5,4 +5,85 @@ class BulkControllerTest < ActionDispatch::IntegrationTest
     patch bulk_url
     assert_response :success
   end
+
+  test 'patch /bulk with two associated operations should success' do
+    params = {
+      "operations": [{
+        "op": "add",
+        "ref": {
+          "type": "authors"
+        },
+        "data": {
+          "type": "authors",
+          "lid": "a",
+          "attributes": {
+            "name": "dgeb"
+          }
+        }
+      }, {
+        "op": "add",
+        "ref": {
+          "type": "articles"
+        },
+        "data": {
+          "type": "articles",
+          "attributes": {
+            "title": "JSON API paints my bikeshed!"
+          },
+          "relationships": {
+            "author": {
+              "data": {
+                "type": "authors",
+                "lid": "a"
+              }
+            }
+          }
+        }
+      }]
+    }
+
+    patch bulk_url, params: params
+    assert_response :success
+  end
+  test 'patch /bulk with two associated with wrong operations order should success' do
+    params = {
+      "operations": [
+      {
+        "op": "add",
+        "ref": {
+          "type": "articles"
+        },
+        "data": {
+          "type": "articles",
+          "attributes": {
+            "title": "JSON API paints my bikeshed!"
+          },
+          "relationships": {
+            "author": {
+              "data": {
+                "type": "authors",
+                "lid": "a"
+              }
+            }
+          }
+        }
+      },
+      {
+        "op": "add",
+        "ref": {
+          "type": "authors"
+        },
+        "data": {
+          "type": "authors",
+          "lid": "a",
+          "attributes": {
+            "name": "dgeb"
+          }
+        }
+      }]
+    }
+
+    patch bulk_url, params: params
+    assert_response :success
+  end
 end
